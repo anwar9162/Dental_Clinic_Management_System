a6ccc1dfde8fe69c814a37f69a7d0c2f794e34da
amirkeyire <anwarahmed9162@gmail.com>
2024-08-16 23:39:59 +0300

    search by phone and mark selected patient added

--- lib/screens/appointment/patient_list_widget.dart
+++ lib/screens/appointment/patient_list_widget.dart
@@ -18,6 +18,7 @@ class _PatientListWidgetState extends State<PatientListWidget> {
   final TextEditingController _searchController = TextEditingController();
   List<Patient> _allPatients = [];
   List<Patient> _filteredPatients = [];
+  Patient? _selectedPatient; // Variable to keep track of the selected patient
 
   @override
   void initState() {
@@ -33,7 +34,9 @@ class _PatientListWidgetState extends State<PatientListWidget> {
       _filteredPatients = _allPatients.where((patient) {
         final fullName =
             '${patient.firstName} ${patient.lastName}'.toLowerCase();
-        return fullName.contains(query);
+        final phone = patient.phoneNumber?.toLowerCase() ??
+            ''; // Ensure phone is not null
+        return fullName.contains(query) || phone.contains(query);
       }).toList();
     });
   }
@@ -43,9 +46,37 @@ class _PatientListWidgetState extends State<PatientListWidget> {
     return BlocBuilder<PatientBloc, PatientState>(
       builder: (context, state) {
         if (state is PatientLoading) {
-          return Center(child: CircularProgressIndicator());
+          return Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 8.0,
+                  spreadRadius: 2.0,
+                ),
+              ],
+            ),
+            child: Center(child: Text('Loading...')),
+          );
         } else if (state is PatientError) {
-          return Center(child: Text('Error: ${state.message}'));
+          return Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 8.0,
+                  spreadRadius: 2.0,
+                ),
+              ],
+            ),
+            child: Center(child: Text('Error: ${state.message}')),
+          );
         } else if (state is PatientLoaded) {
           // Store all patients only when they are loaded
           if (_allPatients.isEmpty) {
@@ -76,6 +107,15 @@ class _PatientListWidgetState extends State<PatientListWidget> {
                     labelText: "Search Patients",
                     border: OutlineInputBorder(),
                   ),
+                  onTap: () {
+                    if (_selectedPatient != null) {
+                      // Pre-fill the search bar with the selected patient's name
+                      _searchController.text =
+                          '${_selectedPatient!.firstName} ${_selectedPatient!.lastName}';
+                      _searchController.selection = TextSelection.fromPosition(
+                          TextPosition(offset: _searchController.text.length));
+                    }
+                  },
                 ),
                 SizedBox(height: 8.0),
                 Expanded(
@@ -83,12 +123,32 @@ class _PatientListWidgetState extends State<PatientListWidget> {
                     children: _filteredPatients.isEmpty
                         ? [Center(child: Text('No patients found'))]
                         : _filteredPatients.map((patient) {
+                            final isSelected = patient == _selectedPatient;
                             return ListTile(
+                              leading: isSelected
+                                  ? Container(
+                                      width: 5.0,
+                                      color: Colors.blue,
+                                    )
+                                  : null,
+                              tileColor: isSelected
+                                  ? Colors.blue.shade100
+                                  : Colors.transparent,
                               title: Text(
                                 '${patient.firstName} ${patient.lastName}',
-                                style: TextStyle(fontWeight: FontWeight.normal),
+                                style: TextStyle(
+                                  fontWeight: isSelected
+                                      ? FontWeight.bold
+                                      : FontWeight.normal,
+                                ),
                               ),
+                              subtitle: patient.phoneNumber != null
+                                  ? Text('Phone: ${patient.phoneNumber}')
+                                  : null,
                               onTap: () {
+                                setState(() {
+                                  _selectedPatient = patient;
+                                });
                                 widget.onPatientSelected(patient);
                               },
                             );
@@ -99,7 +159,21 @@ class _PatientListWidgetState extends State<PatientListWidget> {
             ),
           );
         } else {
-          return Center(child: Text('No patients available'));
+          return Container(
+            padding: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12.0),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 8.0,
+                  spreadRadius: 2.0,
+                ),
+              ],
+            ),
+            child: Center(child: Text('No patients available')),
+          );
         }
       },
     );
